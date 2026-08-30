63d0c7848f753a6c3b6bb9f5d8e05b266e651ea9
Fodi9159 <Fodi9159@users.noreply.github.com>
2026-08-30 13:08:12 +0700

    Fn+V boost toggle: toast with on/off icons, action selectable in F-row bindings

--- app/Properties/Resources.Designer.cs
+++ app/Properties/Resources.Designer.cs
@@ -935,6 +935,26 @@ namespace GHelper.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_cpu_boost_96 {
+            get {
+                object obj = ResourceManager.GetObject("icons8_cpu_boost_96", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap icons8_cpu_boost_off_96 {
+            get {
+                object obj = ResourceManager.GetObject("icons8_cpu_boost_off_96", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
         internal static System.Drawing.Icon standard {
